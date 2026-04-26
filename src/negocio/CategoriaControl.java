5c4e507f24d33846cdb9beda6ed7ed76c290fdeb
Jjose17 <chiquibar2007rgmail.com>
2026-04-26 17:22:53 -0500

    Feat: Corregido el error de CategoriaControl

--- src/negocio/CategoriaControl.java
+++ src/negocio/CategoriaControl.java
@@ -66,18 +66,33 @@ public class CategoriaControl {
         }
     }
     
-    public String actualizar(int id, String nombre, String nombreAnt, String descripcion){
-        if(nombre.equals(nombreAnt)){
+  public String actualizar(int id, String nombre, String nombreAnt, String descripcion){
+    if(nombre.equals(nombreAnt)){
+        obj.setId(id);
+        obj.setNombre(nombre);
+        obj.setDescripcion(descripcion);
+        
+        if(DATOS.actualizar(obj)){   // 👈 CORREGIDO
+            return "OK";
+        }else{
+            return "Error en la actualizacion";
+        }
+    }else{
+        if(DATOS.existe(nombre)){
+            return "El registro ya existe";
+        }else{
             obj.setId(id);
             obj.setNombre(nombre);
             obj.setDescripcion(descripcion);
-            if(DATOS.activar(id)){
+            
+            if(DATOS.actualizar(obj)){   // 👈 CORREGIDO
                 return "OK";
             }else{
                 return "Error en la actualizacion";
             }
         }
     }
+}
     
     public String desactivar(int id){
         if(DATOS.desactivar(id)){
