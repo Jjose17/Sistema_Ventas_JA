62b19ab0fb813c9b5744663cd72731e90e621ee9
Juan Jose Acevedo Melo <jjacevedo@ucompensar.edu.co>
2026-05-07 10:18:37 -0500

    Feat entidades: Creamos las entidades del sistema

---
+++ src/entidades/DetalleIngreso.java
@@ -0,0 +1,46 @@
+
+package entidades;
+
+public class DetalleIngreso {
+   
+    private int id;
+    private int ingresoId;
+    private int articuloId;
+    private String articuloCodigo;
+    private String articuloNombre;
+    private int cantidad;
+    private double precio;
+    private double subtotal;
+
+    public DetalleIngreso() {
+    }
+
+    public DetalleIngreso(int id, int ingresoId, int articuloId, String articuloCodigo, String articuloNombre, int cantidad, double precio, double subtotal) {
+        this.id = id;
+        this.ingresoId = ingresoId;
+        this.articuloId = articuloId;
+        this.articuloCodigo = articuloCodigo;
+        this.articuloNombre = articuloNombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.subtotal = subtotal;
+    }
+
+    public DetalleIngreso(int articuloId, int cantidad, double precio) {
+        this.articuloId = articuloId;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public DetalleIngreso(int articuloId, String articuloCodigo, String articuloNombre, int cantidad, double precio, double subtotal) {
+        this.articuloId = articuloId;
+        this.articuloCodigo = articuloCodigo;
+        this.articuloNombre = articuloNombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.subtotal = subtotal;
+    }
+    
+    
+    
+}
